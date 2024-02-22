99d4202b393e0af801f0657466cf2a871a66ca72
Eduardo Aquinta <eddie.ashton@digital.cabinet-office.gov.uk>
2024-02-22 16:31:41 +0000

    try new code for looking at csvs

--- main.py
+++ main.py
@@ -15,25 +15,19 @@ with st.form("Ingest"):
     user_url = st.text_input("Enter the url")
     st.form_submit_button("Ingest", on_click=click_button)
 
+st.text("useful urls:")
+st.text('https://www.ons.gov.uk/peoplepopulationandcommunity/leisureandtourism#datasets')
+
 
 def web_lookup():
     url = user_url
     page = requests.get(url)
-
     soup = BeautifulSoup(page.content, "html.parser")
-    results = soup.find(id="ResultsContainer")
-    python_jobs = results.find_all("h2", string=lambda text: "python" in text.lower())
-    python_job_elements = [
-        h2_element.parent.parent.parent for h2_element in python_jobs
-    ]
+    results = soup.find('ul', {'class': 'list--neutral margin-top--half'})
+    ons_datasets = results.find_all("a", {'class': 'underline-link'})
 
-    for job_element in python_job_elements:
-        title_element = job_element.find("h2", class_="title")
-        company_element = job_element.find("h3", class_="company")
-        location_element = job_element.find("p", class_="location")
-        st.text(title_element.text.strip())
-        st.text(company_element.text.strip())
-        st.text(location_element.text.strip())
+    for element in ons_datasets:
+        st.text(element.text.strip())
         st.text("-----")
 
 
