8253a554e0664f55c15545e04f1105e133259278
Eduardo Aquinta <eddie.ashton@digital.cabinet-office.gov.uk>
2024-03-26 16:18:15 +0000

    remove pages and add links to the main page

--- dmcs-auto.py
+++ dmcs-auto.py
@@ -4,10 +4,13 @@ import streamlit as st
 from streamlit_extras.add_vertical_space import add_vertical_space
 
 st.set_page_config(
-    page_title="DCMS Data Pool",
+    page_title="DCMS Data Automation",
     page_icon="\U0001F600"
 )
-st.sidebar.success("Welcome to the DCMS Data Pool.")
+
+with st.sidebar:
+    st.image("./images/DCMS-logo.png")
+    st.success("Welcome to the DCMS Data Ingestion Automation Tool.")
 
 if 'clicked' not in st.session_state:
     st.session_state.clicked = False
@@ -18,26 +21,31 @@ def click_button():
 
 
 with st.form("Ingest"):
-    st.header("Enter the URL you would like to ingest: ")
-    user_url = st.text_input("Enter the url")
+    st.header("Enter the URL you would like to start automating: ", divider="red")
+    user_url = st.text_input("Enter the url here:")
     st.form_submit_button("Ingest", on_click=click_button)
 
 add_vertical_space(3)
 st.text("useful urls:")
 st.text('https://www.ons.gov.uk/peoplepopulationandcommunity/leisureandtourism#datasets')
 add_vertical_space(3)
+st.text("List of potential data resources:")
 
 
 def web_lookup():
     url = user_url
+    url_no_hashtag = user_url.replace("/peoplepopulationandcommunity/leisureandtourism#datasets", "")
     page = requests.get(url)
     soup = BeautifulSoup(page.content, "html.parser")
     results = soup.find('ul', {'class': 'list--neutral margin-top--half'})
-    ons_datasets = results.find_all("a", {'class': 'underline-link'})
-
-    for element in ons_datasets:
-        st.write(element.text.strip())
-
+    links = []
+    for link in results.find_all('a'):
+        if link.has_attr('href'):
+            href = link['href']
+            if href and not href.startswith('#'):
+                links.append(href)
+    for element in links:
+        st.write(url_no_hashtag + element)
 
 
 if st.session_state.clicked:
